cbeaa14f40ba26a4ef8800ea2331bc349b257582
B1teCode <ardasher.gaynazarov@mail.ru>
2023-04-10 14:31:55 +0500

    The project is written in Django using FBV based on MVT design pattern.

---
+++ Store-FBV/products/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from products.models import ProductCategory, Product, Basket
+# Register your models here.
+
+admin.site.register(ProductCategory)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category')
+    search_fields = ('name',)
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
---
+++ Store-FBV/users/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from users.models import User
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from django.contrib import auth, messages
+from products.models import Basket
+# Create your views here.
+
+def login(request):
+
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'title': 'Store - Авторизация',
+        'form': form
+    }
+    return render(request, 'users/login.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Поздравляем! Вы успешно зарегистрированы!')
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'title': 'Store - Регистрация',
+        'form': form
+    }
+    return render(request, 'users/register.html', context)
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'title': 'Store - Профиль',
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user)
+    }
+    return render(request, 'users/profile.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
